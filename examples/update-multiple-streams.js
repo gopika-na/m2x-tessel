fa8b352090f2b6f876503846cf901c958e8279c1
Josh Hollenbeck <josh.hollenbeck@citrusbyte.com>
2015-01-20 14:20:04 -0800

    update examples for tessel, update updateStreams to support an object of streams and params

---
+++ examples/update-multiple-streams.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+//
+// See https://github.com/attm2x/m2x-nodejs/blob/master/README.md#example-usage
+// for instructions
+//
+
+var config = require("./config");
+var M2X = require("m2x-tessel");
+var m2xClient = new M2X(config.api_key);
+var streams = {
+    "temperature": {
+        "unit": {
+            "label": "celsius",
+            "symbol": "C"
+        },
+        "type": "numeric"
+    },
+    "moisture": {
+        "unit": {
+            "label": "percentage",
+            "symbol": "%"
+        },
+        "type": "numeric"
+    }
+};
+
+// Create the stream if it doesn't exist already
+m2xClient.devices.updateStreams(config.device, streams, function(response) {
+    if (response.isError()) {
+        console.log("Cannot create stream:", response);
+        return;
+    }
+
+    var value_temperature = 24;
+    var value_moisture = 5.0;
+    var handleLoop;
+    handleLoop = setInterval(function () {
+        console.log("I'm updating stream values! (Press CTRL + C to stop)");
+
+        var at = new Date().toISOString();
+        var values = {
+            temperature:  [ { value: value_temperature, timestamp: at } ],
+            moisture:  [ { value: value_moisture, timestamp: at } ],
+        };
+
+        // Write the different values into AT&T M2X
+        m2xClient.devices.postMultiple(config.device, values, function(result) {
+            if (result.isError()) {
+                clearInterval(handleLoop);
+                console.log(result.error());
+            }
+        });
+
+        value_temperature += 1;
+        value_moisture += 0.1;
+    }, 5000);
+});
---
+++ examples/update-single.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+//
+// See https://github.com/attm2x/m2x-nodejs/blob/master/README.md#example-usage
+// for instructions
+//
+
+var config = require("./config");
+var M2X = require("m2x-tessel");
+var m2xClient = new M2X(config.api_key);
+var stream = "temperature";
+var stream_params = {
+    "unit": {
+        "label": "celsius",
+        "symbol": "C"
+    },
+    "type": "numeric"
+};
+
+// Create the stream if it doesn't exist already
+m2xClient.devices.updateStream(config.device, stream, stream_params, function(response) {
+    if (response.isError()) {
+        console.log("Cannot create stream:", response);
+        return;
+    }
+
+    var temperature = 24;
+    setInterval(function () {
+        console.log("I'm updating stream values! (Press CTRL + C to stop)");
+
+        // Update the latest stream value to our new value
+        m2xClient.devices.updateStreamValue(config.device, stream, {"value": new_value}, function(result) {
+            if (result.isError()) {
+                console.log(result.error());
+            }
+        });
+
+        new_value += 1;
+    }, 5000);
+});
