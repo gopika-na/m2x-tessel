38fecafcdeec237d7bf1500080f3fc208296ef15
attsdk <att.drt.sdk@gmail.com>
2016-09-20 17:58:14 +0530

    Add/Remove Device to/from Collection Endpoints

---
+++ examples/remove-device-from-collection.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+/*
+ *
+ * This example demonstrates how to remove existing device from collection
+ *
+ * API Documentation:
+ * https://m2x.att.com/developer/documentation/v2/collections#Remove-device-from-collection
+ */
+
+var config = require("./config");
+var M2X = require("../../m2x-tessel");
+var m2x_client = new M2X(config.api_key);
+var DeviceId = config.device;
+var CollectionId = config.collectionId;
+
+console.log("Removing Device from Collection..");
+
+m2x_client.collections.removeDeviceFromCollection(CollectionId, DeviceId, function(response) {
+    if (response.isSuccess()) {
+        console.log("Status Code: ".concat(response.status));
+        console.log("Device Deleted from Collection.");
+    } else {
+        console.log(JSON.stringify(response.error()));
+        console.log("Device not Deleted from Collection.Please Try Again...");
+    }
+});
