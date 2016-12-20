6ec499a62b617d39029adb9f4b03e67f821c95c5
gokulmInfy <gokul_madhavan@infosys.com>
2016-12-20 11:58:09 +0530

    Modified as per review comments

--- examples/iterate-over-devices.js
+++ examples/iterate-over-devices.js
@@ -5,39 +5,43 @@
  * API Documentation:
  * https://m2x.att.com/developer/documentation/v2/device#List-Devices
  *
- * Setup:
- * Replace <M2X-API-KEY> with an M2X API Key
- *
  */
 var config = require("./config");
 var M2X = require("../lib/m2x");
 var m2x_client = new M2X(config.api_key);
 
-m2x_client.devices.list(function(response) {
+var params = {
+    limit: 5 //Variable to limit the number of devices per page.
+};
+m2x_client.devices.list(params,function(response) {
+    console.log("Status Code: ".concat(response.status));
     if (response.isSuccess()) {
         jsonInfo = JSON.parse(response.raw);
         var number_of_pages = jsonInfo.pages;
         var total_devices = jsonInfo.total;
+        var limit = jsonInfo.limit;
         console.log("Total Number of Devices: ", total_devices);
-        print_devices(number_of_pages);
+        print_devices(number_of_pages,limit);
 
     } else {
         console.log(JSON.stringify(response.raw));
     }
 });
 
-function print_devices(number_of_pages) {
+
+function print_devices(number_of_pages,limit) {
     for(var page = 1; page <= number_of_pages; page++) {
         var params = {
-            page: page
+            page: page,
+            limit: limit
         };
         m2x_client.devices.list(params, function (response) {
             if (response.isSuccess()) {
                 i=1;
                 JSON.parse(response.raw).devices.forEach(function (device) {
-                    console.log("Page",page-1, ", Device ",i++ ," :: ", device.name)
+                    console.log("Page",JSON.parse(response.raw).current_page, ", Device ",i++ ," :: ", device.name)
                 });
-                console.log(JSON.parse(response.raw).total, "Devices returned on page", page-1)
+                console.log("****",JSON.parse(response.raw).devices.length, "Devices returned on page", JSON.parse(response.raw).current_page,"****");
             } else {
                 console.log("Status Code: ".concat(response.status));
                 console.log(JSON.stringify(response.raw));
